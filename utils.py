8df42601094f0945f47d20034c6501ddd235ab28
Luca Contalbo <luca.contalbo@gmail.com>
2024-06-14 13:34:51 +0200

    updated config

--- utils.py
+++ utils.py
@@ -2,11 +2,31 @@ import torch
 import random
 import configparser
 import numpy as np
+
 from sklearn.metrics import accuracy_score, f1_score, precision_score, recall_score
+from ast import literal_eval
 
 def get_config():
     config = configparser.ConfigParser()
     config.read('config.ini')
+
+    to_int = ["num_classes", "num_classes_adv", "embed_size", "first_last_avg",\
+            "seed", "batch_size", "max_len_sent", "epochs", "adversarial", "dataset_from_saved",\
+            "injection", "grid_search", "visualize"]
+    to_float = ["weight_decay", "lr"]
+    to_list = ["class_weight"]
+    datasets = ["student_essay", "debate", "m-arg"]
+
+    for el in to_int:
+        config[el] = int(config[el])
+    
+    for el in to_float:
+        config[el] = float(config[el])
+
+    for el in to_list:
+        for dataset_name in datasets:
+            config[dataset_name][el] = literal_eval(config[dataset_name][el])
+
     return config
 
 def get_device():
